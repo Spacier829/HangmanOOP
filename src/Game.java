7825952df41509d722d8ef9842b01e4c3c6d8f38
Spacier829 <mrsimpleeegor@gmail.com>
2024-09-19 17:48:56 +0700

    created project structure

---
+++ src/Game.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+  private static final Random RANDOM = new Random();
+  private static final Scanner SCANNER = new Scanner(System.in);
+  
+}
